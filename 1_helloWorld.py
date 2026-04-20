f2f9ff07db987071e4f91601e6682a1f19501580
Arman Hossen <2armanhossen@gmail.com>
2026-04-21 02:08:55 +0600

    python

--- 1_helloWorld.py
+++ 1_helloWorld.py
@@ -1,13 +1,20 @@
-print("Hello World") #this is a comment
-print("Sit Back Enjoy The show!")
+print("Hello World!") #this is a comment
+# print("Sit Back Enjoy The show!")
+
+# (pound sign or hashtag) used for single line comment
+
+print() # makes a new line
+print(" 1.Sit back")
+print(" 2.Relax")
+print(" 3.And")
+print(" 4.Enjoy the show")
 # to print in python use print() no ';' at end of the statement, 
 
-# by adding a second print statement, we make a new line
 print() # this can crate a new line
 print("I Like Mango")
 print("It's realy good!\n")
 # This is a comment, we use hast tag to comment a line in python
 print("My Name: " + "Arman Hossen Ripon\n")
-# wow, it doesn't create any .exe file after run!!!
+# wow, it doesn't create any .exe file after run!!!  
 
 print("Hi Broo!\nKemon Acho akon, Onek din por python practice!")
--- 2_variables.py
+++ 2_variables.py
@@ -13,66 +13,71 @@
 
 
 #String -----------------------------------------------------------------------------------------
-# a string is a series of text
-print("1.String variable implementation")
+# a string is a series of text,  this can be in between double. or single qoute 
+
+print("--------------1.String variable implementation--------------")
 first_name = "Arman" 
-
-word = "Human"
-print(word)
-
 # this can e double or single qoutes but use "" always for string!
 # if i use the variable without the "" in the print funciton then that will print the variable value 
 print(first_name)
+
 #if i use the "" for the variable then it will print the variable name or not the value of the variable
 
 # f-string:
 # to use formatted string literals, begin a string with f or F before the openning quotatino mark or tripple quotatino mark,
 # inside this string, you can write a Python expression between {variableName} and characters that can refer to the variables or literal vaues
+# w = "World"
+# print(w)
+# print(f"I am inside the {w}\n")
+
 
 # to format the variable in the string we use f before the "", inside the print() , here the f = means format
 print(f"My First Name is {first_name}")
-
 food = "cake"
 # print(f"i like {food}")
+
+mail = "arman@gmail.com"
+print(f"My Fake mail is {mail}\n")
 
 email = "armanhossen"
 gcom = ".gmail.com"
 ycom = ".yahoo.com"
 
+name = "arman"
+num = "123" #i can not use number to concat while {name+num+gcom} all 3 here have to be same datatye , here they are strings
+print(f"Making a email : {name+num+gcom}\n")
+
 print(f"My Email is {email+gcom}")
 print(f"My Email is not {email+ycom}")
-print("-----------------------------------------------------\n")
-
-
 print(f"This is my gmail {email+gcom}")
 print("\n")
 
-#Integer -----------------------------------------------------------------------------------------
+
+#Integer -------------------------------------------------------------------------------------------------------
 # integer is number without any decimal part
-print("2.Integer variable implementation")
-age = 22 # age should not be in "" then it will be string
+print("--------------2.Integer variable implementation--------------")
+age = 23     # age should not be in "" then it will be string
 quantity = 3 
+numOfStuInCls = 50
 
 print(f"I am now {age} years old.")
 print(f"I will buy {quantity} car, Insha Allah.")
-
-numOfStuInCls = 50
 print(f"There is {numOfStuInCls} students in the class!")
-print("-----------------------------------------------------\n")
-
 
 
 #Float -----------------------------------------------------------------------------------------
 # float is number with  decimal part
-print("3.Float variable implementation")
-price = 10.93
+print("--------------3.Float variable implementation--------------")
+price = 100.93
 gpa = 3.50
 distance = 5.5
 
 print(f"My last semester result was {gpa}")
 print(f"The price is ${price}")
 print(f"I have to run {distance}km.")
-print("-----------------------------------------------------\n")
+
+laptopName = "Asus Future Model"
+laptopPrice = 120500.05
 
 
 
@@ -102,4 +107,4 @@ if online:
     print("Arman is online!")
 else:
     print("Arman is offline!")
-print("-----------------------------------------------------\n")
+
