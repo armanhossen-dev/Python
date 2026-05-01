9e23a0715987f54c0730fb666998ff281975d76a
Arman Hossen <2armanhossen@gmail.com>
2026-05-02 04:53:08 +0600

    python
    #add temperature converter, add weight converter, add currency converter

---
+++ 10_Converter.py
@@ -0,0 +1,24 @@
+#add temperature converter, add weight converter, add currency converter
+
+weight = float(input("\nEnter Your Weight: "))
+unit = input("Choose unit (kg or lb): ").lower().strip()
+
+#kg(kilogram) -> metric system
+#lb(pound) -> imperial system 
+#1 kg = 2.2046226lb
+if unit == "kg":
+    #its kG so converting it into pounds
+    weight = weight * 2.205
+    unit = "Lbs"
+    print(f"weight = {round(weight,2)} {unit}.\n")
+
+elif unit == "lb":
+    #its pounds so converting it into kilograms
+    weight = weight / 2.205
+    unit = "Kgs"
+    print(f"weight = {round(weight,2)} {unit}.\n")
+
+else: 
+    print(f"{unit} is not valid")
+
+# print(f"weight = {round(weight,2)} {unit}.\n") this makes error, even if there is unit not valid then it runs so i add this line inside the if statements
