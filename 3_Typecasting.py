03d8b181871d5b43a98f402afc7e56c48dd3bac3
Arman Hossen <2armanhossen@gmail.com>
2026-05-01 06:05:22 +0600

    python

--- 3_Typecasting.py
+++ 3_Typecasting.py
@@ -2,6 +2,60 @@
 # using ->    str(), int(), float(), bool()
 # this is especially useful while handling user input or doing any conversion
 
+
+# new learnigns
+nam = "Arman Hossen Ripon"
+age = 23
+gpa = 3.3
+happy = False
+
+#type function, to get the variables type, type(variable)
+print(f"\nLets See the type of the variables:\nnam = {nam} \ttype: {type(nam)}")
+print(f"age = {age} \t\t\ttype: {type(age)}")
+print(f"gpa = {gpa} \t\t\ttype: {type(gpa)}")
+print(f"happy = {happy} \t\t\ttype: {type(happy)}")
+
+print("\n\n")
+
+# for changing a variables type to another type, we use typecast
+# using str(), int(), float(), bool()
+
+print(f"lets make the 'age' floating number, \nage = {float(age)}\n")
+print(f"lets make the 'age' boolean, \nage = {bool(age)}\n")
+print(f"lets make the 'gpa' integer number, \nage = {int(gpa)}\n")
+print(f"lets make the 'happy' integer number, \nage = {int(happy)}\n")
+
+
+#making a number string then adding a digit into it again make it number
+target = 3504 
+print("target = 3504")
+
+target = str(target)
+print("target = str(target)")
+
+target += "1"
+print(f"target += \"1\" => {target}")
+
+target = int(target)
+print("target = int(target)")
+target += 1
+print(f"target += 1 => {target}")
+print("Sei na!...😎\n\n")
+
+
+#we can use this for checking user give us his name or not! or emply input
+input = ""
+print("input = \"\"")
+isInput = bool(input)
+print(f"isInput = bool(input)\nisInput = {isInput}")
+
+if isInput == False:
+    print("You didn't Entered your input!")
+else:
+    print(f"Your input is : {input}")
+
+print("\n\n\n\n")
+
 name = "Arman Hossen"
 age = 22
 cgpa = 3.5
