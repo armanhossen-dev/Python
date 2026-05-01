a0020d865c1b88a4618b66f7274fe08b90bc1d44
Arman Hossen <2armanhossen@gmail.com>
2026-05-02 01:12:15 +0600

    python

---
+++ 7_math.py
@@ -0,0 +1,14 @@
+import math
+
+print(f"pi = math.pi = {math.pi}")
+print(f"e = math.e = {math.e}")
+
+#sqrt
+print(f"x = 9, √9 = math.sqrt(x) = {math.sqrt(9)}") #3.0
+
+#ceil - it rounds a number up to the nearest int, no matter what the decimal part is
+print(f"x = 3.4, math.ceil(x) = {math.ceil(3.4)}") # 4
+
+#floor - it rounds a number down to the nearest int, (toward negative infinity)
+a = math.floor(-2.2)
+print(f"a = math.floor(-2.2) = {a}")
