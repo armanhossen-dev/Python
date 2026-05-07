779e1a5ffe9e79fce99af785a9e8b0589da06997
Arman Hossen <2armanhossen@gmail.com>
2026-05-08 03:46:54 +0600

    py

---
+++ 22.py
@@ -0,0 +1,5 @@
+# collection = single "variable" used to store multiple values,
+# list = [] ordered and changeable, Duplicates ok
+# set = {} unordered and immutable, but Add, Remove ok, no duplicates
+# Tuple = () ordered and unchangeable,  Duplicates ok,  Faster
+# https://youtu.be/ix9cRaBkVe0?t=8584
