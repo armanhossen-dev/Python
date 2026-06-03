4f2943382be2aa46d4878bbde4ad02177736df02
Arman Hossen <2armanhossen@gmail.com>
2026-06-03 10:26:24 +0600

    dictionary

--- 27_dictionary.py
+++ 27_dictionary.py
@@ -1,1 +1,23 @@
-# https://youtu.be/ix9cRaBkVe0?t=11008 
+# dictionary = a collection of {key:value} pairs
+#              ordered and changeable. No duplicates
+
+# few example for 
+#     key:value pairs
+# -> id:name, item:price, 
+
+#            country:capitals
+capitals = {"Bangladesh":"Dhaka",
+            "Ireland":"Dublin",
+            "Australia":"Canberra",
+            "United Kingdom":"London",
+            "New Zealand":"Wellington",
+            "China":"Beijing",
+            "Korea":"Seoul",
+            "Japan":"Tokyo",
+            "USA":"Washington D.C",
+            "India":"New Delhi",
+            "Pakistan":"Islamabad",
+            "Russia":"Moscow",
+            "Peru":"Lima",
+            "Canada":"Ottawa"}
+
